922d726cac016f7cbc98d1a45ffc90803b2e40bf
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-11-24 06:53:31 +0200

    wip

---
+++ src/app/mod.rs
@@ -0,0 +1,62 @@
+pub mod controller;
+
+use rust_web_server::app::controller::index::IndexController;
+use rust_web_server::app::controller::not_found::NotFoundController;
+use rust_web_server::app::controller::static_resource::StaticResourceController;
+use rust_web_server::header::Header;
+use rust_web_server::request::Request;
+use rust_web_server::response::{Response, STATUS_CODE_REASON_PHRASE};
+use crate::app::controller::favicon::FaviconController;
+use crate::app::controller::index::IndexController;
+use crate::app::controller::not_found::NotFoundController;
+use crate::app::controller::script::ScriptController;
+use crate::app::controller::style::StyleController;
+
+pub struct App {}
+
+impl App {
+    pub fn handle_request(request: Request) -> (Response, Request) {
+        let header_list = Header::get_header_list(&request);
+
+        let mut response: Response = Response::get_response(
+            STATUS_CODE_REASON_PHRASE.n501_not_implemented,
+            Some(header_list),
+            None
+        );
+
+
+        if IndexController::is_matching_request(&request) {
+            response = IndexController::process_request(&request, response);
+            return (response, request)
+        }
+
+        if StyleController::is_matching_request(&request) {
+            response = StyleController::process_request(&request, response);
+            return (response, request)
+        }
+
+        if ScriptController::is_matching_request(&request) {
+            response = ScriptController::process_request(&request, response);
+            return (response, request)
+        }
+
+        if FaviconController::is_matching_request(&request) {
+            response = FaviconController::process_request(&request, response);
+            return (response, request)
+        }
+
+        if StaticResourceController::is_matching_request(&request) {
+            response = StaticResourceController::process_request(&request, response);
+            return (response, request)
+        }
+
+        if NotFoundController::is_matching_request(&request) {
+            response = NotFoundController::process_request(&request, response);
+            return (response, request)
+        }
+
+
+        (response, request)
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod app;
+
 extern crate core;
 
 
