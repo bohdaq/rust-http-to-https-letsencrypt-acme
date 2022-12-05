c7830dc3d970c2c06cc9c0364e86576c808fe28b
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-12-05 06:19:53 +0200

    wip

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+pub mod app;
+
+use std::net::TcpListener;
+use rust_web_server::entry_point::{bootstrap, get_ip_port_thread_count, set_default_values};
+use rust_web_server::server::Server;
+use rust_web_server::symbol::SYMBOL;
+use rust_web_server::thread_pool::ThreadPool;
+
+fn main() {
+    start();
+}
+
+pub fn start() {
+    const VERSION: &str = env!("CARGO_PKG_VERSION");
+    const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+    const REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
+    const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+    const RUST_VERSION: &str = env!("CARGO_PKG_RUST_VERSION");
+    const LICENSE: &str = env!("CARGO_PKG_LICENSE");
+
+    println!("HTTP to HTTPS with LetsEncrypt HTTP verification");
+    println!("Version:       {}", VERSION);
+    println!("Authors:       {}", AUTHORS);
+    println!("Repository:    {}", REPOSITORY);
+    println!("Desciption:    {}", DESCRIPTION);
+    println!("Rust Version:  {}", RUST_VERSION);
+    println!("License:       {}\n\n", LICENSE);
+    println!("RWS Configuration Start: \n");
+    set_default_values();
+    bootstrap();
+    let (ip, port, thread_count) = get_ip_port_thread_count();
+    create_tcp_listener_with_thread_pool(ip.as_str(), port, thread_count);
+}
+
+pub fn create_tcp_listener_with_thread_pool(ip: &str, port: i32, thread_count: i32) {
+    let bind_addr = [ip, SYMBOL.colon, port.to_string().as_str()].join(SYMBOL.empty_string);
+    println!("Setting up {}...", bind_addr);
+
+    let boxed_listener = TcpListener::bind(&bind_addr);
+    if boxed_listener.is_err() {
+        eprintln!("unable to set up TCP listener: {}", boxed_listener.err().unwrap());
+    } else {
+        let listener = boxed_listener.unwrap();
+        let pool = ThreadPool::new(thread_count as usize);
+
+        println!("Hello, HTTP to HTTPS with LetsEncrypt HTTP verification server is up and running: http://{}", &bind_addr);
+
+        for boxed_stream in listener.incoming() {
+            if boxed_stream.is_err() {
+                eprintln!("unable to get TCP stream: {}", boxed_stream.err().unwrap());
+            } else {
+                let stream = boxed_stream.unwrap();
+
+                print!("Connection established, ");
+
+                let boxed_local_addr = stream.local_addr();
+                if boxed_local_addr.is_ok() {
+                    print!("local addr: {}", boxed_local_addr.unwrap())
+                } else {
+                    eprintln!("\nunable to read local addr");
+                }
+
+                let boxed_peer_addr = stream.local_addr();
+                if boxed_peer_addr.is_ok() {
+                    print!(", peer addr: {}\n", boxed_peer_addr.unwrap())
+                } else {
+                    eprintln!("\nunable to read peer addr");
+                }
+
+                pool.execute(move || {
+                    Server::process_request(stream);
+                });
+            }
+        }
+    }
+
+}
